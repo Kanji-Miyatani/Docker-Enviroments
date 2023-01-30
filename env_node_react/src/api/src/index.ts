d22c475b84278db87e4911235a6ee0c8096d7279
MWSERVER\k-miyatani <k-miyatani@mediaware.co.jp>
2023-01-30 14:57:12 +0900

    expressサーバー実装

---
+++ env_node_react/src/api/src/index.ts
@@ -0,0 +1,9 @@
+import express from "express"
+const PORT = 3000;
+const app = express();
+ var server = app.listen(PORT,()=>{
+    console.log(`Kchat server is listening on port ${PORT}`);
+ })
+ app.get('/',(req,res)=>{
+    return res.send("<h1>KChatへようこそ。</h1>");
+ })
