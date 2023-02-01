9c611affc8f8c0696528291ff9c7d45ca51583d7
Kanji Miyatani <kanji09012458@gmail.com>
2023-02-01 23:48:22 +0900

    クライアントにsocket.io導入

--- env_node_react/src/api/src/index.ts
+++ env_node_react/src/api/src/index.ts
@@ -1,9 +1,16 @@
-import express from "express"
+import express from "express";
+import chat from "./lib/chat"
+
 const PORT = 3001;
 const app = express();
+const chatServer = chat(app);
+
  var server = app.listen(PORT,()=>{
     console.log(`Kchat server is listening on port ${PORT}`);
  })
  app.get('/',(req,res)=>{
     return res.send("<h1>KChatへようこそ。</h1>");
  })
+ 
+
+ 
--- env_node_react/src/api/src/lib/chat.ts
+++ env_node_react/src/api/src/lib/chat.ts
@@ -5,7 +5,12 @@ import { Express } from "express";
 export default (app:Express)=>{
 
     const server = http.createServer(app);
-    const { Server } = require("socket.io");
     const io = new Server(server); 
-    
+    //接続確立
+    io.on('connection',(socket)=>{
+        console.log("user connected!");
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+          });
+    })
 }
