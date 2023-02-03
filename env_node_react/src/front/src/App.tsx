ab6c59a61c5394fcde9e1c69ca954e5381c5a9f2
Kanji-Miyatani <k-miyatani@mediaware.co.jp>
2023-02-03 15:16:56 +0900

    reactのホットリロード復活

--- env_node_react/src/front/src/App.tsx
+++ env_node_react/src/front/src/App.tsx
@@ -2,25 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import Room from './Room';
 import { testlog } from './repository/chatRepository';
-import { BrowserRouter, Route,Link,useParams } from 'react-router-dom';
+import { BrowserRouter, Route,Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <Route exact path="/" >
             <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                KCat
-              </p>
+          <Link to='/room/1'>Room1</Link> <Link to='/room/2'>Room22</Link><Link to='/room/3'>Room3</Link>
+            <Route exact path="/" >
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    KCat
+                  </p>
+                
+            </Route>
+            <Route path="/room/:roomId">
+              <Room messages={testlog.messages}/>
+            </Route>
             </header>
-            <Link to='/room/1'>Room1</Link> <Link to='/room/2'>Room2</Link><Link to='/room/3'>Room3</Link>
-        </Route>
-        <Route path="/room/:roomId">
-          <Room messages={testlog.messages}/>
-        </Route>
       </div>
     </BrowserRouter>
   );
