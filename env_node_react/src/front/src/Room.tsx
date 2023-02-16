763826b9d11db43eeb2de1aff0f9e9ab34d7b438
pe-pu <nagasawa.katsuya2022@gmail.com>
2023-02-16 17:16:34 +0900

    甘デジに対応

--- env_node_react/src/front/src/Room.tsx
+++ env_node_react/src/front/src/Room.tsx
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 import { useRef } from "react";
 import { Log } from "./interfaces/Chat";
 import { useSockets } from "./lib/socket.context";
+import React, { Component } from 'react'
+import ReactDOM from "react-dom";
 const Room = () => {
     const { roomId } = useParams<{ roomId: string }>();
     const { socket, messages, setMessages } = useSockets();
@@ -15,14 +17,20 @@ const Room = () => {
         socket.emit("sendMessage", message);
     }
 
-    function pachinko() {
+    function pachinko(type: string) {
+
         let gamecount: number = 0;
         let bonusflg: boolean = false
-        let tousimoney:number
+        let tousimoney: number
+        let num: number = 0
         const randInt = (min: number, max: number) => Math.floor(Math.random() * (max + 1 - min)) + min;
         while (bonusflg == false) {
-            const num = randInt(1, 319)
-
+            if (type == "ama") {
+                num = randInt(1, 99)
+            }
+            else {
+                num = randInt(1, 319)
+            }
             gamecount++;
 
             if (num == 1) {
@@ -31,6 +39,7 @@ const Room = () => {
         }
         tousimoney = gamecount * 55
         console.log(gamecount + '回であたた! 1k18回転だと仮定すると、投資' + tousimoney + '円くらいで当たったみたいです');
+        let x = gamecount + '回であたた! 1k18回転だと仮定すると、投資' + tousimoney + '円くらいで当たったみたいです'
     }
 
     return (
@@ -47,7 +56,8 @@ const Room = () => {
                 }
             </ul>
             <input type="text" ref={messageRef} className="chat-input" /><button className="btn-send" onClick={handleClick}>送信</button>
-            <button className="btn-send" onClick={pachinko}>うんだめし 1/319</button>
+            <button className="btn-send" onClick={() => pachinko("middle")} data-num="middle">うんだめし 1/319</button>
+            <button className="btn-send" onClick={() => pachinko("ama")} data-num="ama">うんだめし 1/99</button>
         </>
     )
 }
